156c9f0f558621dceebd742e5a2aa939c2e93489
Abdinajib123 <cabdicabdulaahi8899@gmail.com>
2026-01-03 14:18:49 +0300

    update

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -9,14 +9,17 @@ import {
   updateUser,
   deleteUser
 } from "../controllers/userController.js";
+import { protect } from "../middlewares/authMiddleware.js";
 const router = express.Router();
 
 // Protected user CRUD routes
-router.get("/", getUsers);
-router.get("/:id", getUserById);
-router.post("/", signup);
-router.patch("/:id", updateUser);
-router.delete("/:id", deleteUser);
+router.get("/", protect, getUsers);
+router.get("/:id", protect, getUserById);
+router.post("/", protect, signup);
+router.patch("/:id", protect, updateUser);
+router.delete("/:id", protect, deleteUser);
+
+// Public authentication routes
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/forgot-password", forgotPassword);
