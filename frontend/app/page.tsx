61cf4ba086e9333fcae9160877f7ba4da6ea79ae
Abdinajib123 <cabdicabdulaahi8899@gmail.com>
2026-04-12 12:15:38 +0300

    update

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -24,6 +24,7 @@ export default function Home() {
         <HostingPackages />
         <AchievementsSection />
         <RecentBlogs />
+        {/* teamwork */}
         <TeamSection />
         <TestimonialsSection />
         <NewsletterSection />
