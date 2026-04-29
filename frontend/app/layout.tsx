dc088864c73c6214aa9349da05fe4940f6d46830
najib-dev <abdulahimuse46@gmail.com>
2026-04-29 13:18:53 +0300

    update

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head>
+      {/* <head>
         <script
           dangerouslySetInnerHTML={{
             __html: `
@@ -44,7 +44,7 @@ export default function RootLayout({
             `,
           }}
         />
-      </head>
+      </head> */}
       <body
         suppressHydrationWarning
         className={`${geistSans.variable} ${geistMono.variable} antialiased overflow-x-hidden`}
